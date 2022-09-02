385c56b26abd8fdf304289c45fb9cd1eeff4f15f
tony23nguyen <96150979+tony23nguyen@users.noreply.github.com>
2022-09-02 11:10:22 -0700

    Update todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const todosController = require('../controllers/todos') 
 const { ensureAuth } = require('../middleware/auth')
-
+// ensureAuth makes sure people log in.
 router.get('/', ensureAuth, todosController.getTodos)
 
 router.post('/createTodo', todosController.createTodo)
